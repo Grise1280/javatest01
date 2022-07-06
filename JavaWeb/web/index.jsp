0cc962006db18db7fd9ec7584e953a5b4dbf6233
Grise1280 <39304354+Grise1280@users.noreply.github.com>
2022-07-06 22:05:38 +0800

    Initial commit

---
+++ JavaWeb/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: TIMI
+  Date: 2022/7/6
+  Time: 3:30
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>艹</title>
+  </head>
+  <body>
+  <h1 style="color: orangered"></h1>
+  </body>
+</html>
---
+++ src/com/atguigu/java/hello.java
@@ -0,0 +1,21 @@
+package com.atguigu.java;
+
+/**
+ * com.atguigu.java - the name of the target package where the new class or interface will be created.
+ * javatest01 - the name of the current project.
+ * ${FILE_NAME} - the name of the PHP file that will be created.
+ * hello - the name of the new file which you specify in the New File dialog box during the file creation.
+ * TIMI - the login name of the current user.
+ * 2022/7/2 - the current system date.
+ * 14:11 - the current system time.
+ * 2022 - the current year.
+ * 07 - the current month.
+ * 02 - the current day of the month.
+ * 14 - the current hour.
+ * 11 - the current minute.
+ * IntelliJ IDEA - the name of the IDE in which the file will be created.
+ * 七月 - the first 3 letters of the month name. Example: Jan, Feb, etc.
+ * 七月 - full name of a month. Example: January, February, etc.
+ */
+public class hello {
+}
